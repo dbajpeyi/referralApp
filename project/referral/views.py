bd2e594e967f9ff09eb7e8687d7bc3c67fe20302
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-01-13 01:13:44 +0530

    adding the app:WIP

---
+++ project/referral/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.http import JsonResponse
+
+# Create your views here.
+
+class JsonBaseView():
+    
+    def send_err(error_msg):
+        return JsonResponse({
+            'msg' : error_msg,
+            'status' : 'error' 
+        })
+
+    def send_json(msg):
+        return JsonResponse({
+            'msg' : msg,
+            'status' : 'ok'
+        })
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, '/login.html', {})
+
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/home/')
+            else:
+                return self.send_err("Your Django Hackathon account is disabled.")
+        else:
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return self.send_error("Invalid login details supplied.")
+
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/home/')

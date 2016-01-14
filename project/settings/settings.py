d25b7c8c7b7949d6ea10ed342a18f518eeb116b6
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-01-14 12:54:50 +0530

    configure templates

--- project/settings/settings.py
+++ project/settings/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -53,6 +53,7 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'settings.urls'
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -118,5 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+   os.path.join(PROJECT_ROOT, '..', 'vendor'), 
+)

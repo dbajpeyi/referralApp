474507d2b9a697e60d15c12dde8a1260579554bd
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-01-14 12:28:11 +0530

    models

--- project/referral/models.py
+++ project/referral/models.py
@@ -2,8 +2,30 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 
 class Profile(User):
 
+    ext_id  = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    points  = models.PositiveIntegerField()
+
+
+    def __unicode__(self):
+        return self.name
+
+
+class Referral(models.Model):
+
+    """
+    A referral is a link/code that can be used by other users to login and gives points to its user
+    """
+
+    ext_id  = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user    = models.ForeignKey('Profile', on_delete=models.CASCADE)    
+
+
+
+    def __unicode__(self):
+        return self.ext_id
